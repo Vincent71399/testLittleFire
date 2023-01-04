975ace7b9a71ee73a296a132191c1dce84d382f3
vincent <vincent71399@gmail.com>
2023-01-04 12:17:27 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+       <Text>result</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
